bcb1941e0f89f339e32b60b9d0498b38e555e020
tru-advise <prakash.truadvise@gmail.com>
2026-01-27 20:29:53 +0400

    Add files via upload

---
+++ vite.config.js
@@ -0,0 +1,13 @@
+JavaScript
+import path from "path"
+import react from "@vitejs/plugin-react"
+import { defineConfig } from "vite"
+
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+})
